6f62c6dba9515744b9aacbfe0c57e3fa2065b4eb
BlackCoder56 <blackcoda56@gmail.com>
2025-10-26 11:01:36 +0300

    Cards

---
+++ src/components/card1.vue
@@ -0,0 +1,22 @@
+<template>
+    <h3>Data Structures and Algorithms</h3>
+    <b>Data Structures</b>
+    <ul style="list-style: none;">
+        <li>Arrays</li>
+        <li>Set</li>
+        <li>Priority Queue</li>
+    </ul>
+     <b>Algorithms</b>
+    <ul style="list-style: none;">
+        <li>Searching</li>
+        <li>Sorting</li>
+        <li>Greedy</li>
+    </ul>
+</template>
+
+<script>
+    export default {
+        name: 'App',
+        components: {},
+    };
+</script>
---
+++ src/components/card2.vue
@@ -0,0 +1,22 @@
+<template>
+    <h3>Programming Languages</h3>
+    <b>Object Oriented Programming Language</b>
+    <ul style="list-style: none;">
+        <li>Java</li>
+        <li>C++</li>
+        <li>Python</li>
+    </ul>
+    <b>Procedure Oriented Programming Language</b>
+    <ul style="list-style: none;">
+        <li>COBOL</li>
+        <li>FORTRAN</li>
+        <li>C</li>
+    </ul>
+</template>
+
+<script>
+    export default {
+        name: 'App',
+        components: {},
+    };
+</script>
